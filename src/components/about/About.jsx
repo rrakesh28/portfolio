0428799526a6e7ba812664b6402a9a13a3b2fab2
rakeshrepos <rebbavarapurakesh@gmail.com>
2022-09-05 06:55:48 -0400

    updated the image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me1.jpg";
+import ME from "../../assets/me.jpg";
 import { FaAward } from "react-icons/fa";
 const About = () => {
   return (
